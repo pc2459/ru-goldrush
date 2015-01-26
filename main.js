e174211bf58d749e2f33f89d4d87496ea133a82f
Po Linn Chia <chiapolinn@gmail.com>
2015-01-26 12:09:18 -0700

    Get add markers working

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 $(document).on('ready', function() {
+
+	// Get the X,Y position when we click on the .map div
+	// using pageX and pageY from the event (called e)
+
+	$(".map").on("click",function(e){
+		console.log(e.pageX);
+		console.log(e.pageY);
+
+		var marker = $('<div class="marker"><img src="marker.png"></div>');
+
+		$(this).append(marker);
+		marker.css({
+			position: "absolute",
+			top: e.pageY - 64,
+			left: e.pageX - 32
+		});		
+
+	});
   
 });
